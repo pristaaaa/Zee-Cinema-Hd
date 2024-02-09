85bf2d49f596b6effc32a4300cc0663d11bd51bb
myradio <154856702+pristaaaa@users.noreply.github.com>
2024-02-09 19:47:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ jwplayer("video").setup({
   height: "100%",
   autostart: true,
   mute: false,
-  file: "https://cdn-tv.co.in/myweb/watch.php?id=1000009275&e=.m3u8"
+  file: "https://pubads.g.doubleclick.net/ssai/event/xYKVDAMWTQykwzEAWr3luQ/master.m3u8"
 });
